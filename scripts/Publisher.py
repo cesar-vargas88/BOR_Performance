f828ba98e0a35842f215027e1a86d049fa58950a
cesar-vargas88 <vargas.cesar88@gmail.com>
2018-08-22 21:04:36 -0500

    Nodes improved

--- scripts/Publisher.py
+++ scripts/Publisher.py
@@ -1,16 +1,32 @@
 import rospy
-from std_msgs.msg import String
 from performance_tests.msg import SuperAwesome
+from performance_tests.srv import Rate
+
+Counter     = 0
+LoopRate    = 1
+
+def Rate_Callback(req):
+    global LoopRate
+    global Counter 
+    LoopRate = req.data
+    Counter = 0
+    print "Loop rate: %d" %(LoopRate)    
+    return True
 
 def talker():
-    pub = rospy.Publisher('SuperAwesome_Topic', SuperAwesome, queue_size=10)
     rospy.init_node('Publisher_Python', anonymous=True)
-    rate = rospy.Rate(10) 
+    SuperAwesome_Publisher  = rospy.Publisher('SuperAwesome_Topic', SuperAwesome, queue_size=10)
+    Rate_Server             = rospy.Service('Change_Rate_Service', Rate, Rate_Callback)
+    rate = rospy.Rate(1) 
 
     while not rospy.is_shutdown():
-        hello_str = "hello world"
+        global LoopRate
+        global Counter 
+        Counter += 1
+        hello_str = str(Counter)
         rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+        SuperAwesome_Publisher.publish(hello_str)
+        rate = rospy.Rate(LoopRate) 
         rate.sleep()
 
 if __name__ == '__main__':
--- scripts/Subscriber.py
+++ scripts/Subscriber.py
@@ -1,9 +1,8 @@
 import rospy
-from std_msgs.msg import String
 from performance_tests.msg import SuperAwesome
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "%s", data.data)
+    rospy.loginfo("%s", data.data)
  
 def listener():
     rospy.init_node('Subscriber_Python', anonymous=True)
